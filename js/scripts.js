dff40170786b7e22840040ec7e6443df6cb7fc33
Talita Bernardino <talibernardi@outlook.com>
2021-06-22 15:39:08 -0300

    adiciona layout e loop trufas

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$.getJSON("json/trufas.json", function(data){
+    $.each(data.items, function(i, item) {
+        $('#trufas').append('<div class="col-4">'+
+                '<img class="img-fluid"'+
+                    'src="'+item.imageUrl+'"'+
+                    'alt="Trufa Meio Amargo">'+
+            '</div>'+
+            '<div class="col-4">'+
+                '<p><strong>'+item.name+'</strong></p>'+
+               '<p class="form-text">R$ '+item.price+'</p>'+
+                '<p><strong>R$ '+item.sellingPrice+'</strong></p>'+
+            '</div>'+
+            '<div class="col-4">'+
+                '<button class="btn btn-danger">Remover</button>'+
+                '<button class="btn btn-primary">Adicionar</button>'+
+            '</div>')
+    });
+});
