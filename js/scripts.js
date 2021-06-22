4b4b2129644afe91c0e8a0561fcf79a7d391862d
Talita Bernardino <talibernardi@outlook.com>
2021-06-22 19:48:38 -0300

    laço para novo template de imagens

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
+var trufas = {}
+
 $.getJSON("json/trufas.json", function(data){
     $.each(data.items, function(i, item) {
+        trufas[item.uniqueId] = item
+
         $('#trufas').append('<div class="col-4">'+
-                '<img class="img-fluid"'+
-                    'src="'+item.imageUrl+'"'+
-                    'alt="Trufa Meio Amargo">'+
-            '</div>'+
-            '<div class="align-self-center col-4">'+
-                '<p><strong>'+item.name+'</strong></p>'+
-               '<p class="form-text">R$ '+item.price+'</p>'+
-                '<p><strong>R$ '+item.sellingPrice+'</strong></p>'+
-            '</div>'+
-            '<div class="align-self-center col-4">'+
-                '<button class="btn btn-danger">Remover</button>'+
-                '<button class="btn btn-primary">Adicionar</button>'+
-            '</div>')
+            '<img class="img-fluid"'+
+                'src="'+item.imageUrl+'"'+
+                'alt="'+item.name+'">'+
+        '</div>'+
+        '<div class="align-self-center p-4 col-4">'+
+            '<p><strong>'+item.name+'</strong></p>'+
+            '<p class="form-text">R$ '+item.price+'</p>'+
+            '<p><strong>R$ '+item.sellingPrice+'</strong></p>'+
+        '</div>'+
+        '<div class="align-self-center p-4 col-4">'+
+            '<button class="btn btn-danger btn-add">Remover</button>'+
+            '<button class="btn btn-primary btn-rm">Adicionar</button>'+
+        '</div>')
     });
 });
 
