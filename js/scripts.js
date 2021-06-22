a013fda84dfe695bf0ee026e838f567fac6f4bcd
Talita Bernardino <talibernardi@outlook.com>
2021-06-22 19:36:58 -0300

    tamanho img e botoes

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,15 @@ $.getJSON("json/trufas.json", function(data){
                     'src="'+item.imageUrl+'"'+
                     'alt="Trufa Meio Amargo">'+
             '</div>'+
-            '<div class="col-4">'+
+            '<div class="align-self-center col-4">'+
                 '<p><strong>'+item.name+'</strong></p>'+
                '<p class="form-text">R$ '+item.price+'</p>'+
                 '<p><strong>R$ '+item.sellingPrice+'</strong></p>'+
             '</div>'+
-            '<div class="col-4">'+
+            '<div class="align-self-center col-4">'+
                 '<button class="btn btn-danger">Remover</button>'+
                 '<button class="btn btn-primary">Adicionar</button>'+
             '</div>')
     });
 });
+
