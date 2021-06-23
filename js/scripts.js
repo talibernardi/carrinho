32c88fd2ddc36cf48fbca5cc0bfa80f9dc0abd2b
Talita Bernardino <talibernardi@outlook.com>
2021-06-22 21:06:38 -0300

    adiciona remove do carrinho

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var trufas = {}
+var myBag = [];
 
 $.getJSON("json/trufas.json", function(data){
     $.each(data.items, function(i, item) {
         trufas[item.uniqueId] = item
-
         $('#trufas').append('<div class="col-4">'+
             '<img class="img-fluid"'+
                 'src="'+item.imageUrl+'"'+
@@ -15,9 +15,50 @@ $.getJSON("json/trufas.json", function(data){
             '<p><strong>R$ '+item.sellingPrice+'</strong></p>'+
         '</div>'+
         '<div class="align-self-center p-4 col-4">'+
-            '<button class="btn btn-danger btn-add">Remover</button>'+
-            '<button class="btn btn-primary btn-rm">Adicionar</button>'+
+            '<button class="btn btn-primary add" data-id="'+item.uniqueId+'">Adicionar</button>'+
         '</div>')
     });
+
+    addTrufa()
 });
 
+function addTrufa() {
+    $('.add').click(function () {
+        let item = trufas[$(this).data('id')]
+        myBag.push(item)
+        var ids = Object.keys(myBag);
+        var id = ids[ids.length - 1];
+        $('#my-bag').append('<li>'+item.name+'<span class="float-end">R$ '+item.sellingPrice+'<button data-id="'+id+'" type="button" class="btn-close rm"></button></span></li>')
+        countTotal()
+        rmTrufa()
+    })
+}
+
+function rmTrufa() {
+    $('.rm').click(function () {
+        let id = $(this).data('id')
+        $(this).parent().parent().remove()
+        myBag = myBag.filter(function(v, i) {
+            return i != id
+        })
+        countTotal()
+    })
+}
+
+function countTotal() {
+    let total = 0
+    $.each(myBag, function(i, item) {
+        total += item.sellingPrice
+    });
+    
+    total.toFixed(2)
+    if (total >= 10) {
+        $('#alerta-frete').removeClass('d-none')
+    } else {
+        $('#alerta-frete').addClass('d-none')
+    }
+
+    $('#valor-trufa').html('R$ '+total)
+}
+
+addTrufa()
